02794b901a963df0aa8480e245df12b0dfbac31a
shivanginaidu <1001shivangi.naidu@gmail.com>
2023-05-22 15:55:29 +0530

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+export const getPlacesData = async (type, sw, ne) => {
+    try {
+        const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+          params: {
+            bl_latitude: sw.lat,
+            tr_latitude: ne.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
+          },
+          headers: {
+            'X-RapidAPI-Key': '9656cf0facmsh93a62a6e92ecfabp1b6ec7jsn60f35b812eaf',
+            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+          }
+        }); 
+             
+      return data;
+    } catch (error) {
+        console.log(error)
+    }
+}
+export const getWeatherData = async (lat, lng) => {
+  try{
+    const {data} = await axios.get('https://open-weather-map27.p.rapidapi.com/weather', {
+    params: {lon: lng, lat: lat},  
+    headers: {
+        'X-RapidAPI-Key': '9656cf0facmsh93a62a6e92ecfabp1b6ec7jsn60f35b812eaf',
+        'X-RapidAPI-Host': 'open-weather-map27.p.rapidapi.com'
+      }
+    });
+    return data;
+  } catch (error) {
+    console.log(error)
+  }
+}
